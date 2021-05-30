cd4287900308a94185fbc7848c49f3d20bb1bc55
Tanner <fossta@oregonstate.edu>
2021-05-30 15:03:58 -0700

    wrote code to pass first test, forgot to commit, wrote second test to return Fizz if number is multiple of 3

--- Q1/fb.py
+++ Q1/fb.py
@@ -1,1 +1,22 @@
+# def fb():
+#     for i in range(1, 100):
+#         if i % 3 == 0 and i % 5 == 0:
+#             print("FizzBuzz")
+#             return "Fizzbuzz"
+#         elif i % 3 == 0:
+#             print("Fizz")
+#             return "Fizz"
+#         elif i % 5 == 0:
+#             print("Buzz")
+#             return "Buzz"
+#         else:
+#             print(i)
+#             return i
 
+
+# fb()
+
+
+def fb_3_5(i):
+    if i % 3 == 0 and i % 5 == 0:
+        return "FizzBuzz"
--- Q1/fb_tests.py
+++ Q1/fb_tests.py
@@ -6,6 +6,9 @@ class TestCase(unittest.TestCase):
     def test_3s_and_5s(self):
         self.assertEqual(fb_3_5(15), "FizzBuzz")
 
+    def test_3s(self):
+        self.assertEqual(fb_3(9), "Fizz")
+
 
 if __name__ == "__main__":
     unittest.main()
