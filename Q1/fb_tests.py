c8ab0fb4dbb1e00079094f01c18a248dda9b960b
Tanner <fossta@oregonstate.edu>
2021-05-30 15:02:16 -0700

    wrote first test, returns FizzBuzz if number is multiple of 3 and 5

--- Q1/fb_tests.py
+++ Q1/fb_tests.py
@@ -1,1 +1,11 @@
+import unittest
+from fb import *
 
+
+class TestCase(unittest.TestCase):
+    def test_3s_and_5s(self):
+        self.assertEqual(fb_3_5(15), "FizzBuzz")
+
+
+if __name__ == "__main__":
+    unittest.main()
