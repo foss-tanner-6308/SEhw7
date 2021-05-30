318b86d22d932cc3f81dcebce87aafd9e1b507cc
Tanner <fossta@oregonstate.edu>
2021-05-30 15:05:01 -0700

    wrote code to pass second test

--- Q1/fb.py
+++ Q1/fb.py
@@ -1,22 +1,8 @@
-# def fb():
-#     for i in range(1, 100):
-#         if i % 3 == 0 and i % 5 == 0:
-#             print("FizzBuzz")
-#             return "Fizzbuzz"
-#         elif i % 3 == 0:
-#             print("Fizz")
-#             return "Fizz"
-#         elif i % 5 == 0:
-#             print("Buzz")
-#             return "Buzz"
-#         else:
-#             print(i)
-#             return i
-
-
-# fb()
-
-
 def fb_3_5(i):
     if i % 3 == 0 and i % 5 == 0:
         return "FizzBuzz"
+
+
+def fb_3(i):
+    if i % 3 == 0:
+        return "Fizz"
