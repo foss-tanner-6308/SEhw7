cccdd28a7e2f1931d367e85302e71f6ecf05d5eb
Tanner <fossta@oregonstate.edu>
2021-05-31 20:38:57 -0700

    wrote second test, 60 should not be a leap year

--- Q2/ly_tests.py
+++ Q2/ly_tests.py
@@ -6,6 +6,9 @@ class TestCase(unittest.TestCase):
     def test_one(self):
         self.assertEqual(lpy_1(2000), "2000 is a leap year")
 
+    def test_two(self):
+        self.assertEqual(lpy_2(60), "60 is not a leap year")
+
 
 if __name__ == "__main__":
     unittest.main()
