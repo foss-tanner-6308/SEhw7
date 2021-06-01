205f77a1e8291b0f4c803ae4255254dfc00a137b
Tanner <fossta@oregonstate.edu>
2021-05-31 20:36:26 -0700

    changed function name that is called in test file, wrote code to pass first test

--- Q2/ly.py
+++ Q2/ly.py
@@ -1,1 +1,3 @@
-
+def lpy_1(i):
+    if i % 4 == 0 and i % 100 == 0 and i % 400 == 0:
+        return "{} is a leap year".format(i)
--- Q2/ly_tests.py
+++ Q2/ly_tests.py
@@ -4,7 +4,7 @@ from ly import *
 
 class TestCase(unittest.TestCase):
     def test_one(self):
-        self.assertEqual(lpy(2000), "2000 is a leap year")
+        self.assertEqual(lpy_1(2000), "2000 is a leap year")
 
 
 if __name__ == "__main__":
