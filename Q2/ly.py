271c0ef8a4101145ff922fabcc2a54fd6b3824ea
Tanner <fossta@oregonstate.edu>
2021-05-31 20:46:31 -0700

    wrote code to pass third test, 3 is not a leap year

--- Q2/ly.py
+++ Q2/ly.py
@@ -6,3 +6,8 @@ def lpy_1(i):
 def lpy_2(i):
     if i % 100 == 0 and i % 400 != 0:
         return "{} is not a leap year".format(i)
+
+
+def lpy_3(i):
+    if i % 4 != 0:
+        return "{} is not a leap year".format(i)
