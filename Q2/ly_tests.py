7863a8b3e1b684bff1a681aae504468ec134d9c3
Tanner <fossta@oregonstate.edu>
2021-05-31 20:30:31 -0700

    wrote first test, the year 2000 should return that it is a leap year

--- Q2/ly_tests.py
+++ Q2/ly_tests.py
@@ -1,1 +1,11 @@
+import unittest
+from ly import *
 
+
+class TestCase(unittest.TestCase):
+    def test_one(self):
+        self.assertEqual(lpy(2000), "2000 is a leap year")
+
+
+if __name__ == "__main__":
+    unittest.main()
